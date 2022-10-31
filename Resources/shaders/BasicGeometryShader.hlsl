a84d593be5db97f0bc52d2bf3b3d2ba0edfa8891
Yuushiro_Iwata <k021g1138@g.neec.ac.jp>
2022-10-31 11:11:02 +0900

    三角形を出力

--- Resources/shaders/BasicGeometryShader.hlsl
+++ Resources/shaders/BasicGeometryShader.hlsl
@@ -1,19 +1,32 @@
 #include "BasicShaderHeader.hlsli"
 
-//点→点を出力するサンプル
-[maxvertexcount(1)]//最大出力頂点数
+//点→三角形を出力するサンプル
+[maxvertexcount(3)]//最大出力頂点数
 void main(
 	point VSOutput input[1] : SV_POSITION, //線とか色々ある
-	//点ストリーム
-	inout PointStream< GSOutput > output//ここで出力
+	//三角形ストリーム
+	inout TriangleStream< GSOutput > output//ここで出力
 )
 {
 	
 		GSOutput element;					//出力用頂点データ
 		//頂点情報
-		element.svpos = input[0].svpos;		//頂点座標をコピー
+		//共通
 		element.normal = input[0].normal;	//法線をコピー
 		element.uv = input[0].uv;			//UVをコピー
+		
+		//1点目
+		element.svpos = input[0].svpos;		//頂点座標をコピー
+		//頂点を1個出力する(出力リストに追加)
+		output.Append(element);
+
+		//2点目
+		element.svpos = input[0].svpos + float4(10.0f, 10.0f, 0.0f, 0.0f);		//頂点座標をコピー
+		//頂点を1個出力する(出力リストに追加)
+		output.Append(element);
+
+		//3点目
+		element.svpos = input[0].svpos + float4(10.0f, 0.0f, 0.0f, 0.0f);		//頂点座標をコピー
 		//頂点を1個出力する(出力リストに追加)
 		output.Append(element);
 }
