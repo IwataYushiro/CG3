06f27b21ede8d7bcb267656b638781f38241fbb7
Yuushiro_Iwata <k021g1138@g.neec.ac.jp>
2022-10-31 10:32:30 +0900

    途中でUV加工した三角形

--- Resources/shaders/BasicGeometryShader.hlsl
+++ Resources/shaders/BasicGeometryShader.hlsl
@@ -1,32 +1,23 @@
 #include "BasicShaderHeader.hlsli"
 
-//三角形→線分を3つ出力するサンプル
-[maxvertexcount(4)]//最大出力頂点数
+//三角形→三角形を出力するサンプル
+//(途中でUV加工してタイリング)
+[maxvertexcount(3)]//最大出力頂点数
 void main(
 	triangle VSOutput input[3] : SV_POSITION, //線とか色々ある
-
-	//点ストリーム
-	inout LineStream< GSOutput > output//ここで出力
+	//三角形ストリーム
+	inout TriangleStream< GSOutput > output//ここで出力
 )
 {
-
-	GSOutput element;					//出力用頂点データ
-
-	//三角形の頂点を1点ずつ扱う
 	for (uint i = 0; i < 3; i++)
 	{
+		GSOutput element;					//出力用頂点データ
 		//線分の始点
 		element.svpos = input[i].svpos;		//頂点座標をコピー
 		element.normal = input[i].normal;	//法線をコピー
-		element.uv = input[i].uv;			//UVをコピー
+		element.uv = input[i].uv*2.0f;			//UVを2倍に
 		//頂点を1個出力する(出力リストに追加)
 		output.Append(element);
 	}
-	//最初の点をもう一つ追加
-	element.svpos = input[0].svpos;		//頂点座標をコピー
-	element.normal = input[0].normal;	//法線をコピー
-	element.uv = input[0].uv;			//UVをコピー
-	//頂点を1個出力する(出力リストに追加)
-	output.Append(element);
-
+	
 }
