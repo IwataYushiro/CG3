fcf4087654007b20bb11021098c4aec45be665bb
Yuushiro_Iwata <k021g1138@g.neec.ac.jp>
2022-10-31 10:47:14 +0900

    プリミティブの複製をした三角形表示

--- Resources/shaders/BasicGeometryShader.hlsl
+++ Resources/shaders/BasicGeometryShader.hlsl
@@ -1,23 +1,38 @@
 #include "BasicShaderHeader.hlsli"
 
-//三角形→三角形を出力するサンプル
+//三角形→三角形を2つ出力するサンプル
 //(途中でUV加工してタイリング)
-[maxvertexcount(3)]//最大出力頂点数
+[maxvertexcount(6)]//最大出力頂点数
 void main(
 	triangle VSOutput input[3] : SV_POSITION, //線とか色々ある
 	//三角形ストリーム
 	inout TriangleStream< GSOutput > output//ここで出力
 )
 {
+	//三角形1つ目
 	for (uint i = 0; i < 3; i++)
 	{
 		GSOutput element;					//出力用頂点データ
 		//線分の始点
 		element.svpos = input[i].svpos;		//頂点座標をコピー
 		element.normal = input[i].normal;	//法線をコピー
-		element.uv = input[i].uv*2.0f;			//UVを2倍に
+		element.uv = input[i].uv;			//UVをコピー
 		//頂点を1個出力する(出力リストに追加)
 		output.Append(element);
 	}
-	
+	//現在のストリップを終了
+	output.RestartStrip();
+
+	//三角形2つ目
+	for (uint i = 0; i < 3; i++)
+	{
+		GSOutput element;					//出力用頂点データ
+		//線分の始点
+		element.svpos = input[i].svpos
+			+ float4(20.0f, 0.0f, 0.0f, 0.0f);//頂点座標をX方向に20ずらす
+		element.normal = input[i].normal;	//法線をコピー
+		element.uv = input[i].uv * 5.0f;			//UVを5倍に
+		//頂点を1個出力する(出力リストに追加)
+		output.Append(element);
+	}
 }
