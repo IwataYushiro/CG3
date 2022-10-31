806e2950677f448c04900fdeb91f6100a0f456bb
Yuushiro_Iwata <k021g1138@g.neec.ac.jp>
2022-10-31 10:21:20 +0900

    三角形→線分を3つ出力するサンプル

--- Resources/shaders/BasicGeometryShader.hlsl
+++ Resources/shaders/BasicGeometryShader.hlsl
@@ -1,7 +1,7 @@
 #include "BasicShaderHeader.hlsli"
 
-//三角形→線分を1つ出力するサンプル
-[maxvertexcount(2)]//最大出力頂点数
+//三角形→線分を3つ出力するサンプル
+[maxvertexcount(6)]//最大出力頂点数
 void main(
 	triangle VSOutput input[3] : SV_POSITION, //線とか色々ある
 
@@ -9,22 +9,37 @@ void main(
 	inout LineStream< GSOutput > output//ここで出力
 )
 {
-	//for (uint i = 0; i < 3; i++)
-	//{
-		GSOutput element;					//出力用頂点データ
 
+	GSOutput element;					//出力用頂点データ
+
+	for (uint i = 0; i < 3; i++)
+	{
 		//線分の始点
-		element.svpos = input[0].svpos;		//頂点座標をコピー
-		element.normal = input[0].normal;	//法線をコピー
-		element.uv = input[0].uv;			//UVをコピー
+		element.svpos = input[i].svpos;		//頂点座標をコピー
+		element.normal = input[i].normal;	//法線をコピー
+		element.uv = input[i].uv;			//UVをコピー
 		//頂点を1個出力する(出力リストに追加)
 		output.Append(element);
 
 		//線分の終点
-		element.svpos = input[1].svpos;		//頂点座標をコピー
-		element.normal = input[1].normal;	//法線をコピー
-		element.uv = input[1].uv;			//UVをコピー
+		if (i == 2)
+		{
+			//+1すると溢れるので、最初に戻る
+			element.svpos = input[0].svpos;		//頂点座標をコピー
+			element.normal = input[0].normal;	//法線をコピー
+			element.uv = input[0].uv;			//UVをコピー
+		}
+		else
+		{
+			element.svpos = input[i+1].svpos;		//頂点座標をコピー
+			element.normal = input[i+1].normal;	//法線をコピー
+			element.uv = input[i+1].uv;			//UVをコピー
+		}
+		
 		//頂点を1個出力する(出力リストに追加)
 		output.Append(element);
-	//}
+		//現在のストリップを終了し、次のストリップを開始
+		output.RestartStrip();
+		
+	}
 }
