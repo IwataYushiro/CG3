591ba52f42701af5e9fcb316a0128ff870d04600
Yuushiro_Iwata <k021g1138@g.neec.ac.jp>
2022-10-31 10:08:08 +0900

    三角形→線分を1つ出力するサンプル

--- Resources/shaders/BasicGeometryShader.hlsl
+++ Resources/shaders/BasicGeometryShader.hlsl
@@ -1,22 +1,30 @@
 #include "BasicShaderHeader.hlsli"
 
-//三角形→点を3つ出力するサンプル
-[maxvertexcount(3)]//最大出力頂点数
+//三角形→線分を1つ出力するサンプル
+[maxvertexcount(2)]//最大出力頂点数
 void main(
 	triangle VSOutput input[3] : SV_POSITION, //線とか色々ある
 
 	//点ストリーム
-	inout PointStream< GSOutput > output//ここで出力
+	inout LineStream< GSOutput > output//ここで出力
 )
 {
-	for (uint i = 0; i < 3; i++)
-	{
+	//for (uint i = 0; i < 3; i++)
+	//{
 		GSOutput element;					//出力用頂点データ
-		element.svpos = input[i].svpos;		//頂点座標をコピー
-		element.normal = input[i].normal;	//法線をコピー
-		element.uv = input[i].uv;			//UVをコピー
 
-		//頂点をi個出力する(出力リストに追加)
+		//線分の始点
+		element.svpos = input[0].svpos;		//頂点座標をコピー
+		element.normal = input[0].normal;	//法線をコピー
+		element.uv = input[0].uv;			//UVをコピー
+		//頂点を1個出力する(出力リストに追加)
 		output.Append(element);
-	}
+
+		//線分の終点
+		element.svpos = input[1].svpos;		//頂点座標をコピー
+		element.normal = input[1].normal;	//法線をコピー
+		element.uv = input[1].uv;			//UVをコピー
+		//頂点を1個出力する(出力リストに追加)
+		output.Append(element);
+	//}
 }
